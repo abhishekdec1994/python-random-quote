8200bf355fb76d860d9df99e0c11694aaa42d3a2
abhishekdec1994 <abhisheksachdeva1994@gmail.com>
2020-05-20 10:08:02 +0000

    Tested the Lecture 7 Cases

--- get-quote.py
+++ get-quote.py
@@ -4,10 +4,16 @@ def main2():
 
   f = open("quotes.txt")
   quotes = f.readlines()
-  f.close()
+  
   last = 13
   rnd = random.randint(0,last)
-  print(quotes[rnd])
-
+  lstrip = quotes[rnd].strip()
+  print(quotes[0])
+  f.close()
+  f = open("quotes.txt", 'a')
+  inp = "Please enter "
+  print(inp)
+  f.write(inp)
+  f.close()
 if __name__== "__main__":
   main2()
